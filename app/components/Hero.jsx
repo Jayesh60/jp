cf1a6607f1459a369bc5707eda03eab67298aafd
Jayesh Wadhe <jwadhe46@gmail.com>
2024-06-08 15:38:16 +0530

    chore : hero section imgs, online presence marquee section

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -1,27 +1,64 @@
 import { NavigationBar } from "./NavigationBar";
+import { FaPhoneAlt } from "react-icons/fa";
 
 export const Hero = () => {
   return (
-    <div className="w-full bg-whitesmoke">
+    <div className="w-full bg-whitesmoke ">
       <NavigationBar />
+      <div className="relative h-full max-width">
+        <div className="h-[80vh] border-b w-full flex items-center justify-center">
+          <div className="max-w-[740px] mx-auto text-center flex gap-2 justify-center items-center flex-col">
+            <span className="py-2 px-4 text-desc bg-shinywhite border-gradient">
+              <span className="font-semibold">10+</span> Calls received this
+              month
+            </span>
 
-      <div className="h-[70vh] border-b w-full flex items-center justify-center">
-        <div className="max-w-[740px] mx-auto text-center flex gap-2 justify-center items-center flex-col">
-          <span className="py-2 px-4 text-desc bg-shinywhite border-gradient">
-            <span className="font-semibold">10+</span> Calls received this month
-          </span>
-
-          <h1 className="text-[80px] leading-[80px] text-balance font-semibold py-2">
-            For website that generate revenue
-          </h1>
-          <p className="text-balance text-desc">
-            We'll transform your website into a powerful conversion engine that
-            turns visitors into customers, boosting your revenue effortlessly.
-          </p>
-          <button className="btn">
-            Book a free call
-          </button>
+            <h1 className="text-[80px] leading-[80px] text-balance font-semibold py-2">
+              For website that generate revenue
+            </h1>
+            <p className="text-balance text-desc">
+              We'll transform your website into a powerful conversion engine
+              that turns visitors into customers, boosting your revenue
+              effortlessly.
+            </p>
+            <button className="btn flex items-center gap-2">
+              <FaPhoneAlt size={"1em"} />
+              <span>Book a free call</span>
+            </button>
+          </div>
         </div>
+
+        <img
+          src="/hero/img1.png"
+          alt=""
+          className="h-[28%] object-cover aspect-video absolute 2xl:left-0 xl:left-4 top-5 left-8 rounded-lg"
+        />
+        <img
+          src="/hero/img3.png"
+          alt=""
+          className="h-[28%] object-cover aspect-video absolute 2xl:-left-20 xl:-left-14 top-[35%] -left-4 rounded-lg"
+        />
+        <img
+          src="/hero/img1.png"
+          alt=""
+          className="h-[28%] object-cover aspect-video absolute 2xl:left-0 xl:left-4 bottom-5 left-8 rounded-lg"
+        />
+
+        <img
+          src="/hero/img1.png"
+          alt=""
+          className="h-[28%] object-cover aspect-video absolute 2xl:right-0 xl:right-4 top-5 right-8 rounded-lg"
+        />
+        <img
+          src="/hero/img3.png"
+          alt=""
+          className="h-[28%] object-cover aspect-video absolute 2xl:-right-20 xl:-right-14 top-[35%] -right-4 rounded-lg"
+        />
+        <img
+          src="/hero/img1.png"
+          alt=""
+          className="h-[28%] object-cover aspect-video absolute 2xl:right-0 xl:right-4 bottom-5 right-8 rounded-lg"
+        />
       </div>
     </div>
   );
--- app/components/OnlinePresence.jsx
+++ app/components/OnlinePresence.jsx
@@ -1,5 +1,51 @@
+import Marquee from "react-fast-marquee";
+
 function OnlinePresence() {
-  return <div className="py-20 max-width">OnlinePresence</div>;
+  return (
+    <div className="py-20 ">
+      <div className="max-width py-20 flex-col flex gap-8 rounded-xl bg-[#161616]">
+        <Marquee pauseOnHover={false} speed={20}>
+          <Img src="/hero/img1.png" />
+          <Img src="/hero/img2.png" />
+          <Img src="/hero/img3.png" />
+          <Img src="/hero/img4.png" />
+        </Marquee>
+        <Marquee direction="right" pauseOnHover={false} speed={20}>
+          <Img src="/hero/img1.png" />
+          <Img src="/hero/img2.png" />
+          <Img src="/hero/img3.png" />
+          <Img src="/hero/img4.png" />
+        </Marquee>
+      </div>
+
+      <div className="max-width py-10 flex flex-col gap-4 items-center justify-center">
+        <h1 className="title flex items-center gap-2 text-[40px] font-semibold">
+          <span>We don't just create websites, we create experiences</span>{" "}
+          <span>
+            <img src="/icons/triangle.svg" alt="" />
+          </span>
+        </h1>
+        <p className="text-lg w-[80%] text-center text-desc">
+          We believe that a website should be more than just a digital presence.
+          It should be an engaging, immersive journey that captivates and
+          inspires users.
+        </p>
+        <button className="btn">View all projects</button>
+      </div>
+    </div>
+  );
 }
 
 export default OnlinePresence;
+
+const Img = ({ src }) => {
+  return (
+    <div className="px-5">
+      <img
+        src={src}
+        alt=""
+        className="h-[280px] w-full aspect-video object-cover rounded-lg"
+      />
+    </div>
+  );
+};
